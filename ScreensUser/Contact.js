592a71d7a4d836f59c13e218178e6f5940e69fbf
naremx <pacaon.us@gmail.com>
2019-04-12 14:38:55 +0700

    Add file AdvisorEditAll

--- ScreensUser/Contact.js
+++ ScreensUser/Contact.js
@@ -6,16 +6,17 @@ import { Ionicons } from 'react-native-vector-icons'
 const Contact = () => {
     return (
             <LinearGradient colors ={['#87daf3','#a69beb']} style={Styles.Container}>
+            <View style={{alignItems:'center'}}>
                 <View style={Styles.Box}>
                     <Image style={Styles.Image} source={require('../Image/EIDT.png')} />
                     <Text style={{ color:'#a69beb' , fontSize: 17 , textAlign: 'center', fontWeight: 'bold' , marginTop: 30 }}>ศูนย์พัฒนานวัตกรรมและบริการทางวิศวกรรม</Text>
                     <Text style={{ color:'#a69beb' , fontSize: 10 , textAlign: 'center'}}>Engineering Innovative Development and Technology Services</Text>
                     <Text style={{ color:'#777777' , textAlign: 'center' , marginTop: 20}}>ตั้งอยู่ที่ ชั้น 2 อาคาร Memorial Hall  คณะวิศวกรรมศาสตร์ สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง เลขที่ 1 ซอยฉลองกรุง 1 แขวง/เขตลาดกระบัง กรุงเทพฯ 10520</Text>
-                    <Text style={{ color:'#a69beb' , fontSize: 22 , marginTop: 20 , fontWeight: 'bold', }}>CONTACT US</Text>
-                    <Text style={{ color:'#a69beb'}}>เวลาทำการ 8.30-16.30 น.</Text>
-                    <Text style={{ color:'#a69beb' }}>02-329-8186, 02-329-8000 ext. 3520, 063-4796840</Text>
-                    <Text style={{ color:'#a69beb' }}>eidts@kmitl.ac.th, kmitl.eidts@gmail.com</Text>
-                    <View style={{flexDirection: "row" , marginTop: 40 }}>
+                    <Text style={{ color:'#a69beb' , fontSize: 22 , marginTop: 20 , fontWeight: 'bold', marginLeft : 10 }}>CONTACT US</Text>
+                    <Text style={{ color:'#a69beb', marginLeft : 10  }}>เวลาทำการ 8.30-16.30 น.</Text>
+                    <Text style={{ color:'#a69beb', marginLeft : 10  }}>02-329-8186, 02-329-8000 ext. 3520, 063-4796840</Text>
+                    <Text style={{ color:'#a69beb', marginLeft : 10  }}>eidts@kmitl.ac.th, kmitl.eidts@gmail.com</Text>
+                    <View style={{flexDirection: "row", alignItems:'center' , marginTop: 40 }}>
                         <LinearGradient colors={['#87daf3', '#a69beb']} start={{x: 0.0, y: 1.0}} end={{x: 1.0, y: 1.0}} style={Styles.Button}>
                             <Ionicons name="ios-call" size={50} style={{ color:'#fff', marginTop: 12 , marginLeft: 20}} />
                         </LinearGradient>
@@ -27,6 +28,7 @@ const Contact = () => {
                         </LinearGradient>
                     </View>
                 </View>
+            </View>
             </LinearGradient> 
     );
 }
@@ -45,9 +47,8 @@ const Styles = StyleSheet.create({
         width: 370,
         height: 650,
         backgroundColor: '#fff',
-        alignItems: 'center',
-        marginLeft: 10,
         marginTop: 20,
+        alignItems:'center',
         borderRadius: 25,
         shadowColor: '#30C1DD',
         shadowRadius: 10,
@@ -58,7 +59,7 @@ const Styles = StyleSheet.create({
         width: 75,
         height: 75,
         backgroundColor: '#000',
-        marginLeft: 15,
+        marginLeft: 20,
         marginTop: 20,
         borderRadius: 20,
         shadowColor: '#30C1DD',
---
+++ Test/AdvisorSelectMode.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { StyleSheet,View,TouchableOpacity,Image } from 'react-native';
+import { LinearGradient,Constants } from 'expo';
+import { Actions } from 'react-native-router-flux'
+
+
+export default class AdvisorSelectMode extends React.Component{
+   render(){  
+    return(
+        <LinearGradient colors ={['#87daf3','#a69beb']} style={{ paddingTop: Constants.statusBarHeight }}>
+          <View style={Styles.Container}>
+          <View style={{alignItems:'center'}}>
+          <View style={Styles.ContainerContacts}>
+            <View style={{alignItems:'center'}}>
+            <TouchableOpacity onPress={() => Actions.AdvisorEditAllDate()}>
+              <Image style={{ marginTop: 50 }} source={require('../Image/date.png')} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => Actions.AdvisorEditSelectedDate()}>
+              <Image style={{ marginTop: 30 }} source={require('../Image/datesetting.png')} />
+            </TouchableOpacity>
+            
+            <Image style={{ marginTop: 5 }} source={require('../Image/meeting.png')} />
+
+            </View>
+          </View>
+          </View>
+          </View>
+        </LinearGradient>
+    );
+  }
+}
+
+const Styles = StyleSheet.create({
+    Container: {
+        height: '100%' ,
+    },
+    ContainerContacts: {
+      width: 370,
+      height: 600,
+      backgroundColor: 'white',
+      borderRadius: 18,
+      shadowColor: '#30C1DD',
+      shadowRadius: 10,
+      shadowOpacity: 0.6,
+      elevation: 6,
+    },
+});
