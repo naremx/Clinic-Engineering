0715850c88d411bb1b209b86cfe1c71456288f4d
naremx <pacaon.us@gmail.com>
2019-04-14 18:52:32 +0700

    add modal select file

--- Screens/PhoneBook.js
+++ Screens/PhoneBook.js
@@ -1,62 +1,23 @@
 import React from 'react';
 import { StyleSheet,View,Button,Image,Text } from 'react-native';
-import { LinearGradient,Constants,DocumentPicker,ImagePicker } from 'expo';
+import { LinearGradient,Constants } from 'expo';
 
-
+import ModalCardSelectFile from '../ModalScreen/ModalCardSelectFile.js';
 
 class AdvisorEditSelectDate extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      pickerResult: null,
+
     };
   }
-  _pickDocument = async () => {
-    let result = await DocumentPicker.getDocumentAsync({});
-    alert(result.uri);
-    console.log('_pickDocument',result);
-}
-
-_pickImg = async () => {
-  let pickerResult = await ImagePicker.launchImageLibraryAsync({
-    base64: true,
-    allowsEditing: false,
-    aspect: [4, 3],
-  });
-  console.log(pickerResult)
-  this.setState({
-    pickerResult,
-  });
-};
    render(){  
-    let { pickerResult } = this.state;
-    let imageUri = pickerResult ? `data:image/jpg;base64,${pickerResult.base64}` : null;
-    imageUri && console.log({uri: imageUri.slice(0, 100)});
     return(
         <LinearGradient colors ={['#87daf3','#a69beb']} style={{ paddingTop: Constants.statusBarHeight }}>
           <View style={Styles.Container}>
           <View style={{alignItems:'center'}}>
           <View style={Styles.ContainerContacts}>
-              <Button
-              title="Select Document"
-              onPress={this._pickDocument}
-            />
-            <View style={Styles.container}>
-              <Button onPress={this._pickImg} title="Open Picker" />
-              {pickerResult
-                ? <Image
-                    source={{uri: imageUri}}
-                    style={{ width: 200, height: 200 }}
-                  />
-                : null}
-              {pickerResult
-                ? <Text style={Styles.paragraph}>
-                    Keys on pickerResult:
-                    {' '}
-                    {JSON.stringify(Object.keys(pickerResult))}
-                  </Text>
-                : null}
-            </View>
+            <ModalCardSelectFile/>
           </View>
           </View>
           </View>
